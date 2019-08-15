4e31a6e5789ce2d6b6e82630e1a216250739e847
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-08-14 23:29:19 -0300

    Increase coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,14 @@ function test()
         @test output.solution == zeros(2)
         @test output.objective == 0
     end
+
+    @testset "Simple problem" begin
+        nlp = ADNLPModel(x -> x[1]^2 + x[2]^2, ones(2))
+        output = with_logger(NullLogger()) do
+            random_search(nlp)
+        end
+        @test output.objective < 2
+    end
 end
 
 test()
